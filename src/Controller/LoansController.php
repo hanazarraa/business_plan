cc3cc9875208ff476c65c5d57db404725369ca67
Ahmed BG <boughattas50@gmail.com>
2020-05-31 00:45:15 +0100

    synchronising loans

--- src/Controller/LoansController.php
+++ src/Controller/LoansController.php
@@ -83,6 +83,7 @@ class LoansController extends AbstractController
         $entityManager->flush();
         return $this->redirectToRoute('loans');
     }
+    private $remb ;
     /**
      * @Route("/show/{code}", name="showloans")
      */
@@ -102,17 +103,17 @@ class LoansController extends AbstractController
         $taux = $loans[0]->getTaux();
         $duration = $loans[0]->getDuration();
         $diffirence =  date_diff($datefirstpayment,$dateemprunt) ;
-        $diff = $diffirence->days/ 30.417;
-       
+        $diff = (int) ($diffirence->days / 30.417);
+        $listchange= false;
         //------------------variable de business plan ---------------------//
         $annedebut  = $businessSession->getStartyear();
         $moisdebut  =   $businessSession->getStartmonth();
         $moisdebutInteger = $this->transformMounth($moisdebut);
-        
+      
         //-----------------------Fin------------------------------//
         //------------------------echeance-----------------------------------//
         $echancetoppart = $amount * (($taux / 100)/12); //partie top de la formule de subdivision  (Formule Emprunt)
-        $echancedownpart = (1- pow((1+ ($taux/100/12)),-$duration*12+round($diff))); // partie down de la formule de subdivision 
+        $echancedownpart = (1- pow((1+ ($taux/100/12)),-$duration*12+$diff)); // partie down de la formule de subdivision 
         $echance = round($echancetoppart / $echancedownpart,2);
         
         //--------------------------Fin echance-----------------------------// 
@@ -123,12 +124,19 @@ class LoansController extends AbstractController
         $capitalrestantdu[$x] = "0.00"  ;
         }
         for($i=0;$i<$years ; $i++){
+            $remboursmentsynchroniserSum[$i] = "0.00";
+            $fraissynchroniserSum[$i] = "0.00";
+            $restantsynchroniserSum[$i] = "0.00";
         for($x=0;$x<12;$x++){
-            $remboursmentsynchroniser[$i][$x] = "0.00" ;
+            
             $fraissynchroniser[$i][$x] =  "0.00"  ;
             $restantsynchroniser[$i][$x] = "0.00" ;
+           
         }}
-      
+        for($i=0;$i<$years+1 ; $i++){
+            for($x=-1;$x<12;$x++){
+                $remboursmentsynchroniser[$i][$x] = "0.00" ;
+        }}
         //-----------------------------fin d'initialisation -------------------//
        //----------------------------debut de calcul de remboursement si mensualite egale 12--------------------------//
        $diffrenceBPandLoansYear=$startempruntY -  $annedebut   ;
@@ -145,7 +153,7 @@ class LoansController extends AbstractController
       }
      // dump( $capitalrembourse);die();
       
-       //----------------------------fin de calcul-----------------------------// 
+       //----------------------------fin de calcul-----------------------------//
        //-----------------------reformuler les listes -------------------------//
        $pos = 0 ;
        $year = 0 ;
@@ -157,10 +165,11 @@ class LoansController extends AbstractController
        if($pos == 12 ){
        $pos = 0 ;
        $year ++;}}
-     
-       //----------------------2eme reformulation (synchroniser avec le business plan) ----//
+    
+       //----------------------2eme reformulation (synchroniser avec le business plan par années) ----//
        
        $ye = $diffrenceBPandLoansYear  ;
+       
       // dump($finalrembrousement);die();
        for($i = 0 ; $i< $years;$i++){
            for($x = 0 ; $x < 12 ; $x++){
@@ -171,50 +180,135 @@ class LoansController extends AbstractController
          $restantsynchroniser[$i + $ye][$x]  = $finalrestant[$i][$x] ;
          $restantsynchroniserSum[$i + $ye] = array_sum($finalrestant[$i]);
        }}
-       //dump($remboursmentsynchroniser,$remboursmentsynchroniserSum,$fraissynchroniserSum);die();
+       
+       //----------------------attacher les valeur avec le  mois correspendant ------------//
+       if(!($moisdebutInteger  == $startempruntM + 1 &&  $startempruntY == $annedebut)){
+           
+       
+            $Preparedremboursment = $this->decalage($remboursmentsynchroniser,$moisdebutInteger - 1,$years);
+        
+           
+        }
+           
+    
+       else {
+           $listchange = true ;
+           $Preparedremboursment =   $this->exception($remboursmentsynchroniser,$moisdebutInteger - 1,$years);   
+           
+       }
+     
+       $L = $this->preparelistmonth($moisdebutInteger - 1);
+       $defaultlist =['Janv','Fév','Mars','Avril','Mai','Juin','Juil','Aout','Sept','Oct','Nov','Déc'] ;
+       
+       for($i = 0 ; $i< $years;$i++){
+       foreach($defaultlist as $key=>$value){
+          $remboursmentforshow[$i][$value] = $Preparedremboursment[$i][$key];
+               
+       }} 
+      
+      
+      
+       /* if(!($moisdebutInteger  == $startempruntM + 1 &&  $startempruntY == $annedebut)){
+            for($i = 0 ; $i< $years;$i++){
+                for($x = 0 ; $x < 12 ; $x++){
+                  //  $remboursmentforshow[$i][$x-($moisdebutInteger-1)] =$remboursmentsynchroniser[$i][$x];
+        }}
+       }*/
+      
+       //dump($remboursmentsynchroniser,$remboursmentforshow,$L);die();
       //------------------------------fin--------------------------------------//
        //dump($remboursmentsynchroniser);die();
         return $this->render('loans/detail.html.twig' , ['business' =>$businessSession
         ,'remboursment' => $remboursmentsynchroniser,'frais' =>$fraissynchroniser , 'restant' =>  $restantsynchroniser
-        ,'sumremboursment' => $remboursmentsynchroniserSum ,  'sumfrais' => $fraissynchroniserSum
-       ,'sumrestant' => $restantsynchroniserSum ,
+        ,'sumremboursment' => $remboursmentsynchroniserSum ,  'sumfrais' => $fraissynchroniserSum ,'MonthList' => $L
+       ,'sumrestant' => $restantsynchroniserSum ,'remboursmentforshow' => $remboursmentforshow
         ]);
     }
+    public function decalage(Array $Listadecaler , int $indice,int $years){//cette fonction permet de decaler une liste a partire la liste $L 
+        for($x=0 ;$x<$years+1; $x++ ){
+            for($i=0;$i< 12  ; $i++){
+             $remboursment[$x][$i] = "0.00";
+            }}
+          
+        for($i=0;$i<$years;$i++){
+       for($x=0;$x<$indice;$x++){
+            $remboursment[$i][$x] = $Listadecaler[$i+1][$x];
+        }
+       
+        for($x=$indice;$x<12;$x++){
+            $remboursment[$i][$x] = $Listadecaler[$i][$x];
+        }
+    }
+    return $remboursment;
+    }
+    public function exception(Array $Listadecaler , int $indice,int $years){
+        for($x=0 ;$x<$years+1; $x++ ){
+            for($i=0;$i< 12  ; $i++){
+             $remboursment[$x][$i] = "0.00";}}
+        for($i=0;$i<$years;$i++){               
+        for($x=0;$x<$indice;$x++){
+            $remboursment[$i][$x] = $Listadecaler[$i][12-$indice];
+            }
+        for($x=$indice;$x<12;$x++){
+         $remboursment[$i][$x] = $Listadecaler[$i][$x-1];}}        
+        
+ return  $remboursment ;
+    }
+    public function preparelistmonth(int $firstdate){ // cette fonction permet de decaler les mois  a partir de debut de mois d'un business plan
+    $defaultlist =['Janv','Fév','Mars','Avril','Mai','Juin','Juil','Aout','Sept','Oct','Nov','Déc'] ;
+    if($firstdate>0 && $firstdate <12){
+    $indextable = $firstdate ;
+    for($i = 0 ; $i<$indextable;$i++){
+    $partiegauche[$i] = $defaultlist[$i];
+    }
+    for($i = $indextable ; $i<12;$i++){
+        $partiedroite[$i] = $defaultlist[$i];
+        }
+    $finallist=   array_merge($partiedroite,$partiegauche);
+}
+    else{
+        $finallist = $defaultlist;
+    }
+ 
+    return $finallist;
+}
+
+
     public function transformMounth(String $input){
-    if($input ='Jan'){
+    if($input == 'Jan'){
      return   1;
     }
-    if($input ='Feb'){
+    if($input =='Feb'){
         return   2;
        }
-       if($input ='March'){
+       if($input =='March'){
         return   3;
        }
-       if($input ='April'){
+       if($input =='April'){
         return   4;
        }
-       if($input ='May'){
+       if($input =='May'){
         return   5;
        }
-       if($input ='June'){
+       if($input =='June'){
         return   6;
        }
-       if($input ='July'){
+       if($input =='July'){
         return   7;
        }
-       if($input ='Aug'){
+       if($input =='Aug'){
         return   8;
        }
-       if($input ='Sep'){
+       if($input =='Sep'){
         return   9;
        }
-       if($input ='Oct'){
+       if($input =='Oct'){
         return   10;
        }
-       if($input ='Nov'){
+       if($input =='Nov'){
         return   11;
        }
-       if($input ='Dec'){
+       if($input =='Dec'){
         return   12;
        }
     }
